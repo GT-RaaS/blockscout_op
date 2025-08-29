d4b2797a8ad7c3ead118aba7e10536466c33a2c3
sunth2010 <sunth2010@163.com>
2025-08-29 10:19:00 +0800

    feat: update

---
+++ docker-compose/blockout_restart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "docker down:"
+docker compose -f no-services.yml down
+
+echo
+echo "docker up:"
+docker compose -f no-services.yml up -d
+
+echo
+echo "docker ps:"
+docker compose -f no-services.yml ps
