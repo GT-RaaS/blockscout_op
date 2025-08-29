912b85e5894bd80525b60439421f1447c19383d4
sunth2010 <sunth2010@163.com>
2025-08-29 19:00:04 +0800

    22

--- docker-compose/1_blockout_restart.sh
+++ docker-compose/1_blockout_restart.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 echo "docker down:"
-docker compose -f anvil.yml stop
+docker compose -f anvil.yml down
 
 # 检查文件夹是否存在，如果不存在则创建
 if [ ! -d "blockscout-db-data" ]; then
     mkdir -p blockscout-db-data
 fi
+sudo rm -rf blockscout-db-data/*
 
 if [ ! -d "logs" ]; then
     mkdir -p logs
 fi
+sudo rm -rf logs/*
 
 if [ ! -d "redis-data" ]; then
     mkdir -p redis-data
 fi
+sudo rm -rf redis-data/*
 
 echo
 echo "docker up:"
