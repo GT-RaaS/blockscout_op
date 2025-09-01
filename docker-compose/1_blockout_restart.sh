2f5195aa19e30a3486801e4e888cded2b3c9e2d5
sunth2010 <sunth2010@163.com>
2025-09-01 20:33:49 +0800

    17

--- docker-compose/1_blockout_restart.sh
+++ docker-compose/1_blockout_restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "docker down:"
-docker compose -f geth.yml down
+docker compose -f no-services.yml down
 
 # sudo rm -rf blockscout-db-data
 # if [ ! -d "blockscout-db-data" ]; then
@@ -20,8 +20,8 @@ docker compose -f geth.yml down
 
 echo
 echo "docker up:"
-DOCKER_REPO=blockscout-optimism docker compose -f geth.yml up -d
+docker compose -f no-services.yml up --build -d
 
 echo
 echo "docker ps:"
-docker compose -f geth.yml ps
+docker compose -f no-services.yml ps
