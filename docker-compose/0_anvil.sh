bbf9b3467ebac05097a121b7c10d1f3de4e3f94a
sunth2010 <sunth2010@163.com>
2025-09-02 01:07:58 +0800

    1

---
+++ docker-compose/0_anvil.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+anvil --hardfork cancun --port 9545 --chain-id 42069
---
+++ docker-compose/1_blockout_restart.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "docker down:"
+docker compose -f no-services.yml down
+
+# sudo rm -rf blockscout-db-data
+# if [ ! -d "blockscout-db-data" ]; then
+#     mkdir -p blockscout-db-data
+# fi
+
+# sudo rm -rf logs
+# if [ ! -d "logs" ]; then
+#     mkdir -p logs
+# fi
+
+# sudo rm -rf redis-data
+# if [ ! -d "redis-data" ]; then
+#     mkdir -p redis-data
+# fi
+
+echo
+echo "docker up:"
+DOCKER_REPO=blockscout-optimism docker compose -f no-services.yml up -d
+
+echo
+echo "docker ps:"
+docker compose -f no-services.yml ps
