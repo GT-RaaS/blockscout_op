75bad05681583c5e5cbbc0f2b55e850462817f0d
sunth2010 <sunth2010@163.com>
2025-09-01 18:27:01 +0800

    14

--- docker-compose/1_blockout_restart.sh
+++ docker-compose/1_blockout_restart.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 echo "docker down:"
-docker compose -f no-services.yml stop
+docker compose -f geth.yml down
 
-# 检查文件夹是否存在，如果不存在则创建
+sudo rm -rf blockscout-db-data
 if [ ! -d "blockscout-db-data" ]; then
     mkdir -p blockscout-db-data
 fi
 
+sudo rm -rf logs
 if [ ! -d "logs" ]; then
     mkdir -p logs
 fi
 
+sudo rm -rf redis-data
 if [ ! -d "redis-data" ]; then
     mkdir -p redis-data
 fi
 
 echo
 echo "docker up:"
-DOCKER_REPO=blockscout-optimism docker compose -f no-services.yml up -d
+DOCKER_REPO=blockscout-optimism docker compose -f geth.yml up -d
 
 echo
 echo "docker ps:"
-docker compose -f no-services.yml ps
+docker compose -f geth.yml ps
