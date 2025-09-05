acb027cf44a7e5d4ab224ade67670fc8ac0c85c6
sunth2010 <sunth2010@163.com>
2025-09-05 11:23:34 +0800

    feat: update for meteora

--- docker-compose/1_blockout_restart.sh
+++ docker-compose/1_blockout_restart.sh
@@ -10,7 +10,7 @@ sudo chmod -R 777  logs/
 
 echo
 echo "docker up:"
-DOCKER_REPO=blockscout-optimism FRONTEND_DOCKER_TAG=v2.3.1 docker compose -f geth.yml up -d
+DOCKER_REPO=blockscout-optimism docker compose -f geth.yml up -d
 
 echo
 echo "docker ps:"
