e205d0bbac300e89f19dbcf1e735ed2e076a6789
sunth2010 <sunth2010@163.com>
2025-09-08 20:55:57 +0800

    feat: update devnet

--- docker-compose/1_blockout_restart.sh
+++ docker-compose/1_blockout_restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "IP: 43.133.182.153"
+echo "IP: 43.133.22.92"
 
 echo "docker stop:"
 docker compose -f geth.yml stop
@@ -10,7 +10,7 @@ sudo chmod -R 777  logs/
 
 echo
 echo "docker up:"
-DOCKER_REPO=blockscout-optimism FRONTEND_DOCKER_TAG=v2.3.1 docker compose -f geth.yml up -d
+DOCKER_REPO=blockscout-optimism docker compose -f geth.yml up -d
 
 echo
 echo "docker ps:"
